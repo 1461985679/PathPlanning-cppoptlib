6d3fe04ec9ab71be397ea3155290896cf77e7b5d
futaxiaoli <31175353+1461985679@users.noreply.github.com>
2023-11-22 01:09:25 +0800

    Add files via upload
    更新了曲线和栅格地图，还没完成

--- CppNumercialSolvers_test/test5.cpp
+++ CppNumercialSolvers_test/test5.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
+#include<bits/stdc++.h>
 #include <cppoptlib/meta.h>
 #include <cppoptlib/solver/lbfgsbsolver.h>
 #include <cppoptlib/boundedproblem.h>
@@ -21,9 +19,21 @@ struct S_PointXY
     double Y;
     double Psi;
     void see() {
-        cout << "[" << X << "," << Y << "]";
+        cout << "(" << X << "," << Y << "),";
+    }
+    bool operator == (const S_PointXY& n)const {
+        if (X != n.X)return false;
+        if (Y != n.Y)return false;
+        if (Psi != n.Psi)return false;
+        return true;
     }
 };
+
+bool EqualPosition(S_PointXY a, S_PointXY b) {
+    if (a.X != b.X)return false;
+    if (a.Y != b.Y)return false;
+    return true;
+}
 
 template<typename T>
 class GridPathPlanning : public BoundedProblem<T> {
@@ -54,29 +64,24 @@ public:
             theta += 2.0 * PI;
         return theta;
     }
+    
 
-    // 定曲率曲线距离
-    double fixedCruvatureDistance(S_PointXY p0, S_PointXY p1) {
-        return fixedCruvatureDistance(p0.X, p0.Y, p0.Psi, p1.X, p1.Y, p1.Psi);
+
+    // 欧氏距离
+    double EuclideanDistance(S_PointXY pos0, S_PointXY pos1) {
+        return EuclideanDistance(pos0.X, pos0.Y, pos1.X, pos1.Y);
     }
-    double fixedCruvatureDistance(double x0, double y0, double psi0, double x1, double y1, double psi1) {
-        // 调整角度为相对于x轴的角度
-        psi0 = PI / 2 - psi0;
-        psi1 = PI / 2 - psi1;
+    double EuclideanDistance(double x0, double y0, double x1, double y1) {
+        return sqrt(pow(x0 - x1, 2) + pow(y0 - y1, 2)) + 1;// 距离至少为1 否则重复点就没权值了
+    }
 
-        // 计算两点之间的欧氏距离和切线方向角的一半
-        double d = sqrt(pow(x1 - x0, 2) + pow(y1 - y0, 2));
-        double alpha = (psi1 - psi0) / 2;
-
-        // 计算圆弧的半径和圆心角
-        double R = fabs(d / 2 / sin(alpha));
-        double Delta_theta = 2 * alpha;
-
-        // 计算圆弧的长度
-        double L = R * Delta_theta;
-
-        // 返回圆弧的长度
-        return L;
+    // 曲线距离
+    double fixedCruvatureDistance(const vector<S_PointXY>& pathPoints) {
+        double sum = 0;
+        for (int i = 0; i < pathPoints.size() - 1; i++) {
+            sum += EuclideanDistance(pathPoints[i], pathPoints[i + 1]);
+        }
+        return sum;
     }
 
 
@@ -105,7 +110,8 @@ public:
             if (ePosp0p1 > 0.01) {
                 psi1 = Psip0p1;
             }
-            return linkP2P_DirectPart(x0,y0,psi0,x1,y1);
+            cerr << "stright 1" << endl;;
+            return move(linkP2P_DirectPart(x0, y0, psi0, x1, y1));
         }
 
         Ctr1[0] = x0 - sin(psi0) * 2 * Sign(ePsi1);
@@ -114,18 +120,31 @@ public:
         Disp1C1 = sqrt(SQUARE_SUM(y1 - Ctr1[1], x1 - Ctr1[0]));
         if (Disp1C1 < 2 || fabs(fabs(ePsi1) - PI) < 0.1) {
             psi1 = Psip0p1;
-            return linkP2P_DirectPart(x0, y0, psi0, x1, y1);
+            cerr << "stright 2" << endl;;
+            return move(linkP2P_DirectPart(x0, y0, psi0, x1, y1));
         }
 
 
         double Radius = fabs(ePosp0p1 / 2 / sin(ePsi1));
-
+        //返回曲线
         psi1 = ModPI(psi0 + ePsi1 * 2);
-        return RoverCmdTrack;
+
+        vector<S_PointXY>roverTrack = linkP2P_RoverPart(x0, y0, psi0, x1, y1, psi1);
+
+        // 把弧线映射为整数
+    	for(int i=0;i<roverTrack.size();i++){
+    		roverTrack[i].X = round(roverTrack[i].X);
+    		roverTrack[i].Y = round(roverTrack[i].Y);
+    	}
+        // 一个迭代器 用来村unique进行队列去重以后的返回值用以删除多余的节点
+        vector<S_PointXY>::iterator iter = unique(roverTrack.begin(), roverTrack.end(), EqualPosition);
+        roverTrack.erase(iter, roverTrack.end());
+
+        return move(roverTrack);
     }
     // 两点中间经过的直线栅格点 Bresenham算法
     vector<S_PointXY> linkP2P_DirectPart(S_PointXY p0,S_PointXY p1) {
-        return move(linkP2P_DirectPart(p0.x, p0.y,p0.Psi, p1.x, p1.y));
+        return move(linkP2P_DirectPart(p0.X, p0.Y,p0.Psi, p1.X, p1.Y));
     }
     
     vector<S_PointXY> linkP2P_DirectPart(double x0, double y0,double psi0 ,double x1, double y1) {
@@ -142,10 +161,12 @@ public:
         // 循环直到到达终点
         while (true) {
             // 累加当前栅格的权值
-            directTrack.push_back({ xp,yp,psi0 });
+            directTrack.push_back({ double(xp),double(yp),psi0});
             // 如果到达终点，退出循环
             if (xp == x1 && yp == y1) break;
             if (xp < 0 || yp < 0 || xp >= grid.size() || yp >= grid.size()) {
+                cout << "(" << x0 << "," << y0 << "),"<<psi0;
+                cout << "(" << x1 << "," << y1 << "),";
                 cerr << "直线链接法超出边界"<<endl;
                 break;
             }
@@ -161,7 +182,7 @@ public:
             }
         }
         // 返回权值之和   
-        return directTrack;
+        return move(directTrack);
     }
 
     // 定曲率划线法  这里还需要详细更改
@@ -169,12 +190,20 @@ public:
         // move可以节省内存和时间，直接移动这个points而不是复制里面的每个元素
         return move(linkP2P_RoverPart(p0.X,p0.Y,p0.Psi,p1.X,p1.Y,p1.Psi));
     }
-    vector<S_PointXY> linkP2P_RoverPart(double x0,double y0,double psi0,double x1,double y1,double psi1,double radius) {
-        
-        vector<S_PointXY> roverTrack;
+    vector<S_PointXY> linkP2P_RoverPart(double x0, double y0, double psi0, double x1, double y1, double psi1) {
+
+
+        vector<S_PointXY> roverTrack;// 曲线点集
+
         double PsiTmp;
         double PosTmp[2];
         S_PointXY tmp;
+
+
+        double Psip0p1 = atan2(y1 - y0, x1 - x0);
+        double ePsi1 = ModPI(Psip0p1 - psi0);
+        double ePosp0p1 = sqrt(SQUARE_SUM(y1 - y0, x1 - x0));
+        double radius = fabs(ePosp0p1 / 2 / sin(ePsi1));
 
         PosTmp[0] = x0;
         PosTmp[1] = y0;
@@ -183,7 +212,6 @@ public:
         tmp.X = PosTmp[0];
         tmp.Y = PosTmp[1];
         tmp.Psi = PsiTmp;
-        roverTrack.push_back(tmp);
 
         double dis_cur = 1 / radius * Sign(ePsi1);
         double dis_length = fabs(ePsi1) * radius * 2;
@@ -193,7 +221,9 @@ public:
         double PsiNext;
 
         dis_length = fabs(dis_length);
-        StepNum = 360;
+        // 打点精度 StepNum = 打的点的数量-1  因为起点已经被压入了
+        // 这里用直线距离 * 3的数量打点 可以基本保证打到线上所有点
+        StepNum = static_cast<unsigned int>(int(EuclideanDistance(x0, y0, x1, y1) * 3));
         if (fabs(dis_cur) < 1e-5) {
             for (size_t k = 0; k < StepNum; ++k) {
                 if (dis_flag) {
@@ -204,15 +234,12 @@ public:
                     PosTmp[0] -= dis_length * cos(PsiTmp) / StepNum;
                     PosTmp[1] -= dis_length * sin(PsiTmp) / StepNum;
                 }
-                tmp.X = PosTmp[0];
-                tmp.Y = PosTmp[1];
-                tmp.Psi = PsiTmp;
-                roverTrack.push_back(tmp);
+                roverTrack.push_back({ PosTmp[0],PosTmp[1],PsiTmp });
             }
 
         }
         else {
-            StepNum = max(static_cast<unsigned int>(360), static_cast<unsigned int>(fabs(dis_length * dis_cur) / 2));
+            StepNum = max(static_cast<unsigned int>(StepNum), static_cast<unsigned int>(fabs(dis_length * dis_cur) / 2));
             for (j = 0; j < StepNum; ++j) {
                 PsiNext = PsiTmp + dis_length * dis_cur / StepNum;
                 if (dis_flag) {
@@ -224,23 +251,18 @@ public:
                     PosTmp[1] -= (cos(PsiTmp) - cos(PsiNext)) / dis_cur;
                 }
                 PsiTmp = PsiNext;
-                tmp.X = PosTmp[0];
-                tmp.Y = PosTmp[1];
-                tmp.Psi = PsiTmp;
-                roverTrack.push_back(tmp);
+                roverTrack.push_back({ PosTmp[0],PosTmp[1],PsiTmp });
             }
         }
-        return roverTrack;
+        roverTrack.push_back({ x1,y1,PsiTmp });
+        
+
+        return move(roverTrack);
     }
 
     // 计算从(x0, y0)到(x1, y1)的路径上的栅格的权值之和
-    double LinkGridValue(S_PointXY pos0, S_PointXY pos1) {
-        // move可以节省内存和时间，直接移动这个points而不是复制里面的每个元素
-        return LinkGridValue(pos0.X, pos0.Y, pos0.Psi, pos1.X, pos1.Y, pos1.Psi);
-    }
-    double LinkGridValue(int x0, int y0, double psi0, int x1, int y1, double psi1) {
+    double LinkGridValue(vector<S_PointXY>& pathPoints) {
         double sum = 0;
-        vector<S_PointXY> pathPoints = lineateMethod_FixedCurvature(x0, y0, psi0, x1, y1, psi1);
         for (int i = 0; i < pathPoints.size(); i++) {
             int x_ = pathPoints[i].X;
             int y_ = pathPoints[i].Y;
@@ -252,7 +274,7 @@ public:
                 cout << endl;
                 assert(0);
             }
-            else sum += grid[x_][y_];
+            sum += grid[x_][y_];
         }
         return sum;
     }
@@ -272,7 +294,8 @@ public:
         // loss = 两两点之间的(定曲率路线长度 * 两点中间经过的曲线的地图权值)之和
         double sumValue = 0;
         for (int i = 0; i < points.size() - 1; i++) {
-            sumValue += fixedCruvatureDistance(points[i], points[i + 1]) * LinkGridValue(points[i], points[i + 1]);
+            vector<S_PointXY> pathPoints = linkP2P(points[i], points[i + 1]);
+            sumValue += fixedCruvatureDistance(pathPoints) * LinkGridValue(pathPoints);
         }
 
         //cout << sumValue << ",,,\n";
